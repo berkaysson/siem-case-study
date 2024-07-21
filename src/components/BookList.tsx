7715b506f9e91dea3198d68aaa67d9bcc6c459b1
berkaysson <berkaysonel85@gmail.com>
2024-07-21 21:43:02 +0300

    add styles for select tag

--- src/components/BookList.tsx
+++ src/components/BookList.tsx
@@ -8,8 +8,8 @@ const BookList: React.FC = () => {
   const { books, sortMethod, setSortMethod } = useContext(BookContext);
   return (
     <div>
-      <label htmlFor="sortMethod">Sort by:</label>
-      <select
+      <label htmlFor="sortMethod">Sort by: </label>
+      <StyledSelect
         name="sortMethod"
         id="sortMethod"
         value={sortMethod}
@@ -19,7 +19,7 @@ const BookList: React.FC = () => {
         <option value={SortMethod.TitleDesc}>Title (Z-A)</option>
         <option value={SortMethod.AuthorAsc}>Author (A-Z)</option>
         <option value={SortMethod.AuthorDesc}>Author (Z-A)</option>
-      </select>
+      </StyledSelect>
       <StyledBooklist>
         {books.map((book) => (
           <BookItem key={book.id} book={book} />
@@ -42,4 +42,18 @@ const StyledBooklist = styled.ul`
   max-width: 1280px;
   margin: 0 auto;
   width: 100%;
+`;
+
+const StyledSelect = styled.select`
+  padding: 0.5rem;
+  margin-bottom: 1rem;
+  border-radius: 4px;
+  border: none;
+  background-color: ${(props) => props.theme.background};
+  color: ${(props) => props.theme.text};
+  box-shadow: ${(props) => props.theme.boxShadow};
+
+  &:hover {
+    background-color: ${(props) => props.theme.hover};
+  }
 `;
